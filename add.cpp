d385d5c9425f1754a05014d378e8dd712be6654a
cloudcreatr <79000269+cloudcreatr@users.noreply.github.com>
2023-03-06 08:28:11 +0000

    add

---
+++ add.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+int main()
+{
+    int a, b, sum, sub, multi, div;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    
+    printf("Sum = %d \n", a+b);
+    printf("sub = %d \n", a-b);
+    printf("multi = %d \n", a * b);
+    printf("div = %d \n", a / b);
+
+
+    return 0;
+
+   //check the 
+   
+		}
