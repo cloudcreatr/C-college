3dd802a8d6baac5bb497ee8d218a1aaaeb433cb1
cloudcreatr <79000269+cloudcreatr@users.noreply.github.com>
2023-03-13 02:57:53 +0000

    new compiler

--- convert-temp.cpp
+++ convert-temp.cpp
@@ -9,7 +9,7 @@ int main()
     printf("Enter temperature in Celsius: ");
     scanf("%f", &celsius);
     fahrenheit = (celsius * 9 / 5) + 32;
-    printf("%.2f Celsius = %.2f Fahrenheit", celsius, fahrenheit);
+    printf("%.2f Celsius = %.2f Fahrenheit\n", celsius, fahrenheit);
     return 0;
 }
 
